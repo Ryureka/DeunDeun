89fd3d8e70b3f74e485e47ccf597360f28e4c739
정표용 <sur2@naver.com>
2019-10-16 13:46:24 +0900

    test

---
+++ DeunT/src/main/java/com/Deun/DeunTApplication.java
@@ -0,0 +1,13 @@
+package com.Deun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeunTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeunTApplication.class, args);
+	}
+
+}
