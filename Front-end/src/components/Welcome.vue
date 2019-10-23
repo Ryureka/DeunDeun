15be8864e8a82d64498f9cf4440701a15db93063
오명현 <loucks@naver.com>
2019-10-23 09:35:29 +0900

    프론트 제작|카테고리 완료

---
+++ Front-end/src/components/Welcome.vue
@@ -0,0 +1,62 @@
+<template>
+  <section
+    id="welcome"
+    class="hide-overflow"
+  >
+    <v-layout>
+      <v-flex
+        hidden-sm-and-down
+        md6
+      >
+        <v-img
+          :src="require('@/assets/Home.jpg')"
+          height="100.5vh"
+        />
+      </v-flex>
+
+      <v-flex
+        xs12
+        md6
+        align-content-space-between
+        layout
+        :pa-5="$vuetify.breakpoint.smAndDown"
+        wrap
+      >
+        <base-bubble-1
+          style="transform: rotate(180deg) translateY(25%)"
+        />
+
+        <v-layout
+          align-center
+          justify-center
+        >
+          <v-flex
+            xs10
+            md6
+          >
+            <base-heading>Deun Deun</base-heading>
+            <base-text>
+              국밥은 한국 전통의 음식으로<br>
+              과거부터 지금까지 사랑받고 있습니다<br>
+              <br>
+              아침밥같은 든든함으로
+              <br>
+              여러분의 식생활을 책임지겠습니다
+            </base-text>
+            <v-btn color="secondary" rounded class="mt-4" :to="{name:'FoodCategory'}">
+              &nbsp;시작하기&nbsp;
+            </v-btn>
+
+            <v-btn  color="secondary" rounded class="mt-4 ml-5" :to="{name:'FoodCategory'}">
+              &nbsp;로그인&nbsp;
+            </v-btn>
+          </v-flex>
+        </v-layout>
+
+        <base-bubble-2
+          style="transform: rotate(180deg) translate(-200px, -15%)"
+        />
+      </v-flex>
+    </v-layout>
+  </section>
+</template>
