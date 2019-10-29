0e57cc044fe2f47f46f768053a5ef8b40875c0db
정표용 <sur2@naver.com>
2019-10-29 09:44:22 +0900

    머지 요청

--- SpringBootDD/SpringBootDD/src/main/java/com/ssafy/api/controller/RestaurantController.java
+++ SpringBootDD/SpringBootDD/src/main/java/com/ssafy/api/controller/RestaurantController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.api.dto.Restaurant;
@@ -18,24 +19,25 @@ import com.ssafy.api.service.RestaurantService;
 public class RestaurantController {
 	@Autowired
 	private RestaurantService service;
-	
-	@RequestMapping(value="/" ,method = RequestMethod.GET)
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public List<Restaurant> selectAll() {
 		return service.selectAll();
 	}
-	
+
 	@RequestMapping(value = "/{restaurant_id}", method = RequestMethod.GET)
 	public Restaurant selectOneById(@PathVariable int restaurant_id) {
 		return service.selectOneById(restaurant_id);
 	}
-	
+
 	@RequestMapping(value = "/lowest", method = RequestMethod.GET)
-	public List<Restaurant> selectLowestPrice(){
+	public List<Restaurant> selectLowestPrice() {
 		return service.selectLowestPrice();
 	}
-	
-	@RequestMapping(value = "/lon={x}/lat={y}/range={range}", method = RequestMethod.GET)
-	public List<Restaurant> searchRestaurant(@PathVariable double x, @PathVariable double y, @PathVariable int range) {
+
+	@RequestMapping(value = "/here", method = RequestMethod.GET)
+	public List<Restaurant> searchRestaurant(@RequestParam("lon") double x, @RequestParam("lat") double y,
+			@RequestParam("range") int range) {
 		return service.searchRestaurant(x, y, range);
 	}
 }
--- temp_back/deundeun_back/src/main/java/com/deundeun/app/repository/RestaurantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.deundeun.app.repository;
-
-import com.deundeun.app.model.Restaurant;
-
-public interface RestaurantRepository {
-	Restaurant getRestaurant();
-}
