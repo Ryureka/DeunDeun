bccf229c24db2ed4c93c3c85e18b1c6af0001628
정표용 <sur2@naver.com>
2019-10-31 17:48:01 +0900

    maps in vue

--- front-test/src/components/HelloWorld.vue
+++ front-test/src/components/HelloWorld.vue
@@ -1,57 +1,76 @@
 <template>
-  <div>
+  <div id="HelloWorld">
     <div class="hello">
       <h1>{{ msg }}</h1>
     </div>
 
     <h3>My Google Maps Demo</h3>
     <!--The div element for the map -->
-    <p>{{ now_x }}</p>
-    <p>{{ now_y }}</p>
+    <p>lng {{ now_x }} , lat {{ now_y }}</p>
 
-    <div id="map" ref="maps"></div>
+    <div id="map"></div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import gmapsInit from "../utils/gmaps.js";
 
 export default {
   name: "HelloWorld",
   data: function() {
     return {
-      now_x : '',
-      now_y : '',
+      now_x: "",
+      now_y: "",
       restaurant: {}
     };
   },
   props: {
     msg: String
   },
-  created() {
-    
-    
-    },
-  mounted() {
- 
-    
+  created() {},
+  async mounted() {
+    try {
+      const google = await gmapsInit();
+      const geocoder = new google.maps.Geocoder();
+      const map = new google.maps.Map(document.getElementById("map"));
+
+      var Item_1 = new google.maps.LatLng(36.348082038993525, 127.2956595737923);
+
+      var myPlace = new google.maps.LatLng(36.353477961006476, 127.3023592262077);
+
+      var bounds = new google.maps.LatLngBounds();
+      bounds.extend(myPlace);
+      bounds.extend(Item_1);
+      map.fitBounds(bounds);
+      map.setCenter(Item_1);
+
+      /* geocoder.geocode({ address: "South Korea" }, (results, status) => {
+        if (status !== "OK" || !results[0]) {
+          throw new Error(status);
+        }
+
+        map.setCenter(results[0].geometry.location);
+        map.fitBounds(results[0].geometry.viewport);
+        console.log(results);
+      }); */
+    } catch (error) {
+      console.error(error);
+    }
+
     axios({
       url: "https://www.googleapis.com/geolocation/v1/geolocate",
       method: "post",
       params: {
-        key : "AIzaSyBjg8cR_tJB7iQCXQHpxzAZNERPK1sR5JQ"
+        key: "AIzaSyBjg8cR_tJB7iQCXQHpxzAZNERPK1sR5JQ"
       }
     }).then(res => {
       console.log("geolocation ", res);
-      this.now_x = res.data.location.Lng,
-      this.now_y = res.data.location.Iat
+      this.now_x = res.data.location.lng;
+      this.now_y = res.data.location.lat;
     });
-
   },
-  methods : {
-      
-  }
-
+  methods: {}
 };
 </script>
 
@@ -72,16 +91,9 @@ a {
   color: #42b983;
 }
 
-
 /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
+#map {
+  height: 500px;
+}
 </style>
